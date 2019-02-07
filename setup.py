162752fd45a3703c1abdf607312c345bdee720b8
René Fritze <rene.fritze@wwu.de>
2019-02-07 15:29:07 +0100

    change package prefix from flask->sanic

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@
 Flask-GitHubApp
 -------------
 
-Easy GitHub app integration for Flask
+Easy GitHub app integration for Sanic
 """
 import os
 from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), 'flask_githubapp/version.py'), 'r') as f:
+with open(os.path.join(os.path.dirname(__file__), 'sanic_githubapp/version.py'), 'r') as f:
     exec(f.read())
 
 
 setup(
-    name='Flask-GitHubApp',
+    name='Sanic-GitHubApp',
     version=__version__,
     url='https://github.com/bradshjg/flask-githubapp',
     license='MIT',
@@ -20,12 +20,12 @@ setup(
     author_email='james.g.bradshaw@gmail.com',
     description='Rapid GitHub app development in Python',
     long_description=__doc__,
-    packages=['flask_githubapp'],
+    packages=['sanic_githubapp'],
     zip_safe=False,
     include_package_data=True,
     platforms='any',
     install_requires=[
-        'flask',
+        'sanic',
         'github3.py'
     ],
     classifiers=[
